aef2962c45474ba8a49bfb51c70e4c9877973130
Jephthah Ndukwe <69301693+Jephthahndukwe@users.noreply.github.com>
2024-02-28 16:15:39 +0000

    new update

--- src/Dashboard/pages/Website.jsx
+++ src/Dashboard/pages/Website.jsx
@@ -16,25 +16,25 @@ const Website = () => {
            <div className='flex items-center justify-center mt-[6rem]'>
                 <div>
                     <h1 className='text-[30px] font-[500] text-center'>How would you like to design your website?</h1>
-                    <div className='flex gap-[35px] items-center mt-10'>
-                        <div className='bg-[#fff] py-[25px] shadow-2xl hover:shadow-lg hover:shadow-[#00AABC] px-[40px] w-[450px] h-[320px] rounded-[15px]'>
+                    <div className='lg:flex gap-[35px] items-center mt-10'>
+                        <div className='bg-[#fff] py-[25px] shadow-2xl hover:shadow-lg hover:shadow-[#00AABC] px-[40px] lg:w-[450px] xs:w-[100%] lg:h-[320px] rounded-[15px]'>
                             <div className='flex justify-center items-center'>
                                 <img src={Icons1} className='w-[100px]' />
                             </div>
-                            <h3 className='text-[#000624] text-[20px] font-[700] leading-[30px] mt-[2rem] text-center'>Let Adanian Lab create a site for you</h3>
-                            <h4 className='text-[#000624] text-[16px] font-[400] leading-[24px] mt-[3%] text-center'>Use our site-builder to go live quickly.</h4>
+                            <h3 className='text-[#000624] lg:text-[20px] xs:text-[15px] font-[700] leading-[30px] mt-[2rem] text-center'>Let Adanian Lab create a site for you</h3>
+                            <h4 className='text-[#000624] lg:text-[16px] xs:text-[13px] font-[400] leading-[24px] mt-[3%] text-center'>Use our site-builder to go live quickly.</h4>
                             <p className='flex justify-center items-center mt-5'>
                                 <Link to='/' className='text-[16px] text-center font-[500] leading-[24px] text-[#fff] bg-[#00AABC] hover:bg-[#116dff] py-[9px] px-[30px] w-[55%] rounded-[100px]'>Get Started</Link>
                             </p>
                         </div>
-                        <div className='bg-[#fff] shadow-2xl py-[25px] hover:shadow-lg hover:shadow-[#00AABC] px-[40px] w-[450px] h-[320px] rounded-[15px]'>
+                        <div className='bg-[#fff] shadow-2xl py-[25px] hover:shadow-lg hover:shadow-[#00AABC] px-[40px] lg:w-[450px] xs:w-[100%] lg:h-[320px] rounded-[15px] lg:mt-0 xs:mt-[2rem]'>
                             <div className='flex justify-center items-center'>
                                 <img src={Icons2} className='w-[100px]' />
                             </div>
-                            <h3 className='text-[#000624] text-[20px] font-[700] leading-[30px] mt-[2rem] text-center'>Customize a designed template</h3>
-                            <h4 className='text-[#000624] text-[16px] font-[400] leading-[24px] mt-[3%] text-center'>Choose from thousands of designs for your site.</h4>
+                            <h3 className='text-[#000624] lg:text-[20px] xs:text-[16px] font-[700] leading-[30px] mt-[2rem] text-center'>Customize a designed template</h3>
+                            <h4 className='text-[#000624] lg:text-[16px] xs:text-[13px] font-[400] leading-[24px] mt-[3%] text-center'>Choose from thousands of designs for your site.</h4>
                             <p className='flex justify-center items-center mt-5'>
-                                <Link to='/website/templates' className='text-[16px] text-center font-[500] leading-[24px] text-[#fff] bg-[#00AABC] hover:bg-[#116dff] py-[9px] px-[30px] w-[55%] rounded-[100px]'>Pick a Template</Link>
+                                <Link to='/website/templates' className='text-[16px] text-center font-[500] leading-[24px] text-[#fff] bg-[#00AABC] hover:bg-[#116dff] py-[9px] px-[30px] lg:w-[55%] xs:w-[70%] rounded-[100px]'>Pick a Template</Link>
                             </p>
                         </div>
                     </div>
