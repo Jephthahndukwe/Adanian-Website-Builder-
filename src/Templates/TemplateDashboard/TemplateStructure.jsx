b726cbf743ea9a5fee9015c15c0e8aa35d69261e
Jephthah Ndukwe <69301693+Jephthahndukwe@users.noreply.github.com>
2024-02-29 13:48:00 +0000

    new update

--- src/Templates/TemplateDashboard/TemplateStructure.jsx
+++ src/Templates/TemplateDashboard/TemplateStructure.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FaSearch } from "react-icons/fa";
 import OnlineStore1 from '../../assets/images/onlinestore1.png'
 import OnlineStore2 from '../../assets/images/onlinestore2.png'
 import OnlineStore3 from '../../assets/images/onlinestore3.png'
@@ -35,85 +36,173 @@ import Entertainment1 from '../../assets/images/entertainment1.png'
 import Entertainment2 from '../../assets/images/entertainment2.png'
 import Entertainment3 from '../../assets/images/entertainment3.png'
 import Entertainment4 from '../../assets/images/entertainment4.png'
-import PropTypes from 'prop-types';
 
 
 const imageData = {
   'Online Store': [
-    { src: OnlineStore1 },
-    { src: OnlineStore2 },
-    { src: OnlineStore3 },
-    { src: OnlineStore4 },
-    { src: OnlineStore5 },
-    { src: OnlineStore6 },
+    { src: OnlineStore1, text: 'Accessories Store' },
+    { src: OnlineStore2, text: 'Electronics Store' },
+    { src: OnlineStore3, text: 'Stationery Store' },
+    { src: OnlineStore4, text: 'T- Shirt Store' },
+    { src: OnlineStore5, text: 'Beauty Shop' },
+    { src: OnlineStore6, text: 'Supermarket' },
     // Add more online store images
   ],
   'Portfolio': [
-    { src: Portfolio1 },
-    { src: Portfolio2 },
-    { src: Portfolio3 },
-    { src: Portfolio4 },
-    { src: Portfolio5 },
+    { src: Portfolio1, text: 'Photographer' },
+    { src: Portfolio2, text: 'Fashion Designer' },
+    { src: Portfolio3, text: 'Fashion Designer' },
+    { src: Portfolio4, text: 'Graphic Designer' },
+    { src: Portfolio5, text: 'Videographer' },
     // Add more Portfolio images
   ],
   'Blog': [
-    { src: Blog1 },
-    { src: Blog2 },
-    { src: Blog3 },
+    { src: Blog1, text: 'Personal Blog' },
+    { src: Blog2, text: 'Travel Blog' },
+    { src: Blog3, text: 'Personal Blog' },
     // Add more Blog images
   ],
   'Educational': [
-    { src: Educational1 },
-    { src: Educational2 },
-    { src: Educational3 },
+    { src: Educational1, text: 'Online School' },
+    { src: Educational2, text: 'College' },
+    { src: Educational3, text: 'Education Blog' },
     // Add more Educational images
   ],
   'Service Business': [
-    { src: Services1 },
-    { src: Services2 },
-    { src: Services3 },
+    { src: Services1, text: 'Cleaning Company' },
+    { src: Services2, text: 'Home Improvement Company' },
+    { src: Services3, text: 'Recycling Facility' },
     // Add more restaurant images
   ],
   'Restaurant': [
-    { src: Food1 },
-    { src: Food2 },
-    { src: Food3 },
-    { src: Food4 },
+    { src: Food1, text: 'Dinner Restaurant' },
+    { src: Food2, text: 'Restaurant' },
+    { src: Food3, text: 'Farm' },
+    { src: Food4, text: 'Forestry Company' },
     // Add more restaurant images
   ],
   'IT & Technology': [
-    { src: Technology1 },
-    { src: Technology2 },
-    { src: Technology3 },
+    { src: Technology1, text: 'AI Company' },
+    { src: Technology2, text: 'Tech Company' },
+    { src: Technology3, text: 'IT Services Company' },
     // Add more IT & Technology images
   ],
   'HealthCare': [
-    { src: Healthcare1 },
-    { src: Healthcare2 },
+    { src: Healthcare1, text: 'Home Healthcare Company' },
+    { src: Healthcare2, text: 'Medical Clinic' },
     // Add more HealthCare images
   ],
   'Fashion': [
-    { src: Fashion1 },
-    { src: Fashion2 },
-    { src: Fashion3 },
+    { src: Fashion1, text: 'Clothing Store' },
+    { src: Fashion2, text: 'Shoe Store' },
+    { src: Fashion3, text: 'Clothing Store' },
     // Add more Fashion images
   ],
   'Entertainment': [
-    { src: Entertainment1 },
-    { src: Entertainment2 },
-    { src: Entertainment3 },
-    { src: Entertainment4 },
+    { src: Entertainment1, text: 'Event Planning Company' },
+    { src: Entertainment2, text: 'Club' },
+    { src: Entertainment3, text: 'DJ' },
+    { src: Entertainment4, text: 'Event Venue' },
     // Add more Entertainment images
   ],
 };
 
-export const Navbar = ({ setCategory, setSearchQuery }) => {
-  const handleClick = category => {
+export const Navbar = ({ setCategory, setSearchResults  }) => {
+  const [expanded, setExpanded] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  const expandSearch = () => {
+    setExpanded(true);
+  };
+
+  const collapseSearch = () => {
+    setExpanded(false);
+    setSuggestions([]);
+  };
+
+  const handleSearchChange = (e) => {
+    const query = e.target.value.toLowerCase();
+    setSearchQuery(query);
+    
+    const matchedSuggestions = [];
+  
+    // Check category names
+    Object.keys(imageData).forEach(category => {
+      if (category.toLowerCase().includes(query)) {
+        matchedSuggestions.push(category);
+      }
+    });
+  
+    // Check image texts
+    Object.values(imageData).forEach(images => {
+      images.forEach(image => {
+        if (image.text && image.text.toLowerCase().includes(query)) {
+          matchedSuggestions.push(image.text);
+        }
+      });
+    });
+  
+    setSuggestions(matchedSuggestions);
+  };
+
+  const handleSearchSubmit = () => {
+    const resultsSet = new Set(); // Using Set to avoid duplicates
+    const query = searchQuery.toLowerCase();
+    
+    // Search through category names
+    Object.keys(imageData).forEach(category => {
+      if (category.toLowerCase().includes(query)) {
+        imageData[category].forEach(image => resultsSet.add(image)); // Add all images from matching category
+      }
+    });
+    
+    // Search through image texts
+    Object.keys(imageData).forEach(category => {
+      imageData[category].forEach(image => {
+        if (image.text && image.text.toLowerCase().includes(query)) {
+          resultsSet.add(image); // Add matching images
+        }
+      });
+    });
+  
+    const results = [...resultsSet]; // Convert Set back to an array
+    setSearchResults(results);
+    collapseSearch(); // Collapse search bar after search
+  };
+
+
+  const handleClick = (category) => {
     setCategory(category);
+    setSearchResults([]); // Reset search results when clicking on a category
   };
 
   return (
-    <div className="lg:px-[30px] xs:px-[20px] py-[20px] lg:flex justify-between items-center">
+    <div>
+       <div className="py-[50px] lg:px-[40px] xs:px-[20px] lg:flex justify-between items-center">
+            <h1 className="text-[#000000] lg:text-[32px] xs:text-[25px] font-[700]">Pick the Website Template You Love</h1>
+            <label className="flex items-center lg:mt-0 xs:mt-[2rem]">
+                <input 
+                    type="search" 
+                    placeholder="Search all templates..."
+                    value={searchQuery}
+                    onChange={handleSearchChange}
+                    onClick={expandSearch}
+                    onBlur={collapseSearch}
+                    onKeyPress={(e) => {
+                      if (e.key === 'Enter') {
+                        handleSearchSubmit();
+                      }
+                    }}
+                    className="mt-[15px] font-[400] text-[14px] h-[1px] py-[10px] outline-none pb-[25px]" 
+                    style={{ color: `rgba(102, 102, 102, 0.80)`, borderBottom: '1px solid #0AADBF', width: expanded ? '400px' : '300px', transition: 'width 0.3s ease' }}
+                />
+                <FaSearch className="ms-[-1rem] text-[15px]" onClick={handleSearchSubmit} style={{color: `rgba(102, 102, 102, 0.80)` }} />
+                  </label>
+        </div>
+        <hr className="w-[100%] h-[1px] bg-[#eee]"/>
+
+      <div className="lg:px-[30px] xs:px-[20px] py-[20px] lg:flex justify-between items-center">
       <ul className="flex flex-wrap gap-[15px] items-center">
         {Object.keys(imageData).map(category => (
           <li
@@ -129,38 +218,62 @@ export const Navbar = ({ setCategory, setSearchQuery }) => {
         All Templates
       </h2>
     </div>
-  );
-};
-
-const ImageDisplay = ({ category }) => {
-  const images = category === 'All' ? Object.values(imageData).flat() : imageData[category];
-
-
-  return (
-    <div className='lg:px-[50px] xs:px-[20px] mt-[2rem] lg:mb-0 xs:mb-[4rem]'>
-        <h1 className='text-[#000] lg:text-[20px] xs:text-[18px] font-[700] leading-[32px]'>{category} Website Templates</h1>
-        <div className="lg:flex justify-start items-center flex-wrap gap-[2rem] mt-[2rem]">
-        {images.map((image, index) => (
-            <img key={index} src={image.src} alt={category} className="m-2 lg:w-[30%] xs:w-[100%] lg:mt-0 xs:mt-[3rem]" />
-        ))}
-        </div>
     </div>
   );
 };
 
-const TemplateStructure = ({ setSearchQuery }) => {
+const ImageDisplay = ({ category, searchResults, searchQuery  }) => {
+  const images = category === 'All' ? Object.values(imageData).flat() : imageData[category];
+  const displayImages = searchResults.length > 0 ? searchResults : images;
+
+
+  return (
+    <div className='lg:px-[50px] xs:px-[20px] mt-[2rem] lg:mb-0 xs:mb-[4rem] bg-[#f5f7f7] pt-[2rem]'>
+        <h1 className='text-[#000] lg:text-[20px] xs:text-[18px] font-[700] leading-[32px]'>{category} Website Templates</h1>
+        <div className='lg:flex justify-start items-center flex-wrap gap-[2rem] mt-[2rem]'>
+          {displayImages.map((image, index) => {
+            const randomIndex = Math.floor(Math.random() * (index + 1));
+            [images[index], images[randomIndex]] = [images[randomIndex], images[index]];
+            return (
+              <div className=''>
+                  <div className='relative'>
+                  {/* <div className="hover:bg-[#F1F1F1B3] w-[95.8%] h-[80%] text-transparent absolute hover:text-white flex items-center justify-center transition-opacity duration-300 ms-[0.5rem]">
+                    <h1 className='hover:bg-blue-500 rounded-[50px] p-[10px] w-[20%] flex justify-center items-center text-[14px] font-[500]'>Edit</h1>
+                    <h1>view</h1>
+                  </div> */}
+                    <img key={index} src={image.src} alt={`${category}, ${`Image ${index + 1}`}`} className="m-2  lg:mt-0 xs:mt-[3rem]" />
+                    <h1 className='mt-[2rem] mb-[1rem] ms-[0.5rem] text-[#000] text-[16px] leading-[22px] font-[500]'>{image.text}</h1>
+                  </div>
+              </div>
+            )
+          })}
+        </div>
+        {/* <div className="lg:flex justify-start items-center flex-wrap gap-[2rem] mt-[2rem]">
+        {displayImages.map((image, index) => {
+            const randomIndex = Math.floor(Math.random() * (index + 1));
+            [images[index], images[randomIndex]] = [images[randomIndex], images[index]];
+            return (
+              <div>
+                <img key={index} src={image.src} alt={`${category}, ${`Image ${index + 1}`}`} className="m-2 lg:w-[30%] xs:w-[100%] lg:mt-0 xs:mt-[3rem]" />
+                <h1>{image.text}</h1>
+              </div>
+            )
+        })}
+        </div> */}
+    </div>
+  );
+};
+
+const TemplateStructure = () => {
   const [category, setCategory] = useState('All');
+  const [searchResults, setSearchResults] = useState([]);
 
   return (
     <div>
-      <Navbar setCategory={setCategory} setSearchQuery={setSearchQuery} />
-      <ImageDisplay category={category} />
+      <Navbar setCategory={setCategory} setSearchResults={setSearchResults}  />
+      <ImageDisplay category={category} searchResults={searchResults} />
     </div>
   );
 };
-
-TemplateStructure.propTypes = {
-    setSearchQuery: PropTypes.func.isRequired // Ensure setSearchQuery is a function
-  };
 
 export default TemplateStructure;
--- src/Templates/TemplateDashboard/Templates.jsx
+++ src/Templates/TemplateDashboard/Templates.jsx
@@ -1,49 +1,13 @@
 import Navbar from "../../Dashboard/Navbar"
-import { useState } from "react";
-import { FaSearch } from "react-icons/fa";
 import TemplateStructure from "./TemplateStructure";
 
 const Templates = () => {
-    const [expanded, setExpanded] = useState(false);
-    const [searchQuery, setSearchQuery] = useState('');
-
-    const expandSearch = () => {
-      setExpanded(true);
-    };
-  
-    const collapseSearch = () => {
-      setExpanded(false);
-    };
-
-    const handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
-            expandSearch();
-        }
-    };
-
     return (
         <div>
            <div className="shadow-md">
             <Navbar/>
            </div>
-            <div className="py-[50px] lg:px-[40px] xs:px-[20px] lg:flex justify-between items-center">
-                <h1 className="text-[#000000] lg:text-[32px] xs:text-[25px] font-[700]">Pick the Website Template You Love</h1>
-                <label className="flex items-center lg:mt-0 xs:mt-[2rem]">
-                    <input 
-                        type="search" 
-                        placeholder="Search all templates..."
-                        onKeyDown={handleKeyPress}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        onClick={expandSearch}
-                        onBlur={collapseSearch}
-                        className="mt-[15px] font-[400] text-[14px] h-[1px] py-[10px] outline-none pb-[25px]" 
-                        style={{ color: `rgba(102, 102, 102, 0.80)`, borderBottom: '1px solid #0AADBF', width: expanded ? '400px' : '300px', transition: 'width 0.3s ease' }}
-                    />
-                    <FaSearch className="ms-[-1rem] text-[15px]" style={{color: `rgba(102, 102, 102, 0.80)` }} />
-                </label>
-            </div>
-            <hr className="w-[100%] h-[1px] bg-[#eee]"/>
-            <TemplateStructure setSearchQuery={setSearchQuery}/>
+            <TemplateStructure/>
         </div>
     )
 }
